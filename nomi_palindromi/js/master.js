113739b618925cbb008afc2d84ce70d30900caa9
Davide <davide.roberti89@gmail.com>
2020-02-24 14:05:48 +0100

    fine esercizio parola palindroma

--- nomi_palindromi/js/master.js
+++ nomi_palindromi/js/master.js
@@ -1,1 +1,27 @@
 
+// chiedo all'utente di inserire un nome e lo definisco come variabile
+// creo una funzione che ordini le lettere della stringa del nome inserito dall'utente al contrario
+// tramite un if comparo il nome inserito dall'utente con quello ribaltato
+// se i due nomi risultano uguali restituisco true, altrimenti restituisco false
+// tremite un if fuori falla funzione: se true visualizzo a schermo che il nome inserito è palindromo, se false visualizzo che non lo è
+
+var nomeInserito = prompt('inserisci un nome per scoprire se è palindromo');
+
+
+function palindromo()  {
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }
+    var nomeContrario = reverseString(nomeInserito)
+    if (nomeInserito === nomeContrario) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (palindromo(true)) {
+    console.log(nomeInserito + ' è palindromo');
+} else {
+    console.log(nomeInserito + ' non è palindromo');
+}
