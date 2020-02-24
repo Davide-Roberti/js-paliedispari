5e9319ae3deb79c7032594772334731fcee08050
Davide <davide.roberti89@gmail.com>
2020-02-24 14:42:50 +0100

    fine

--- pari_dispari/js/master.js
+++ pari_dispari/js/master.js
@@ -6,3 +6,22 @@
 var scommessaUtente = prompt('scrivi se scommetti su pari o dispari');
 var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 5'));
 var numeroRandom = Math.floor(Math.random() * 5) + 1;
+
+console.log('hai scelto ' + scommessaUtente + ' con numero ' + numeroUtente);
+console.log('l\' intelligenza artificiale ha scelto ' + numeroRandom);
+
+if (pariDispari(true)) {
+    console.log('complimenti, hai vinto!');
+} else {
+    console.log('mi dispiace, hai perso!');
+}
+
+function pariDispari () {
+    var sommaNumeri = numeroUtente + numeroRandom;
+    console.log(sommaNumeri);
+    if ((scommessaUtente.toLowerCase() === 'pari' && sommaNumeri % 2 == 0) || (scommessaUtente.toLowerCase() === 'dispari' && sommaNumeri % 2 == 1)) {
+        return true;
+    } else {
+        return false;
+    }
+}
